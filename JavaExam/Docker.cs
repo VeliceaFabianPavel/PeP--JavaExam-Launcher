f553af164e2a7bf53e7ecf7326c66947229bae92
Fabian <Fabian@DESKTOP-04VR923>
2023-03-26 15:10:58 +0300

    Add project files.

---
+++ JavaExam/Docker.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Header;
+
+namespace JavaExam
+{
+
+	public partial class Docker : Form
+	{
+		[DllImport("user32.dll", SetLastError = true)]
+		private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+		[DllImport("user32.dll", SetLastError = true)]
+		private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int x, int y, int cx, int cy, uint uFlags);
+
+		private const uint SWP_NOZORDER = 0x0004;
+		private const uint SWP_SHOWWINDOW = 0x0040;
+		private TimeSpan timeLeft;
+		public string windowName = "SunAwtFrame";
+		public Docker()
+		{
+			InitializeComponent();
+			this.TopMost = true;
+			KillExplorer();
+			this.StartPosition = FormStartPosition.Manual;
+			this.Width = Screen.PrimaryScreen.WorkingArea.Width;
+			this.Location = new System.Drawing.Point(0, Screen.PrimaryScreen.WorkingArea.Height - this.Height);
+			this.Resize += OnFormResize;
+			this.Move += OnFormMove;
+			this.FormClosed += OnFormClosed;
+			BlockWebsites(); // Add this line
+			DockAppToTop();
+			timeLeft = TimeSpan.FromMinutes(45);
+			label1.Text = timeLeft.ToString(@"hh\:mm\:ss");
+			timer1.Start();
+		}
+		
+		private void DockAppToTop()
+		{
+			IntPtr hWndApp = FindWindow(windowName, null);// For IntelliJ: SunAwtFrame
+			if (hWndApp != IntPtr.Zero)
+			{
+				SetWindowPos(hWndApp, IntPtr.Zero, 0, 0, Screen.PrimaryScreen.WorkingArea.Width,
+					Screen.PrimaryScreen.WorkingArea.Height - this.Height, SWP_NOZORDER | SWP_SHOWWINDOW);
+			}
+			else
+			{
+				MessageBox.Show("Eclipse IDE not found. Please open Eclipse before running this application.","Error launching the Exam",MessageBoxButtons.OK,MessageBoxIcon.Stop);
+				Application.Exit();
+			}
+		}
+
+		private void OnFormMove(object sender, EventArgs e)
+		{
+			DockAppToTop();
+		}
+
+		private void OnFormResize(object sender, EventArgs e)
+		{
+			DockAppToTop();
+		}
+		private void KillExplorer()
+		{
+			try
+			{
+				ProcessStartInfo psi = new ProcessStartInfo
+				{
+					FileName = "cmd.exe",
+					Arguments = "/C TASKKILL /F /IM explorer.exe",
+					WindowStyle = ProcessWindowStyle.Hidden,
+					CreateNoWindow = true,
+					UseShellExecute = false
+				};
+
+				Process.Start(psi);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Error killing explorer.exe: " + ex.Message,"Internal Error",MessageBoxButtons.OK,MessageBoxIcon.Stop);
+			}
+		}
+		private void OnFormClosed(object sender, FormClosedEventArgs e)
+		{
+			UnblockWebsites();
+			IntPtr hWndApp = FindWindow("SunAwtFrame", null);
+			if (hWndApp != IntPtr.Zero)
+			{
+				SetWindowPos(hWndApp, IntPtr.Zero, 0, 0, Screen.PrimaryScreen.WorkingArea.Width,
+					Screen.PrimaryScreen.WorkingArea.Height, SWP_NOZORDER | SWP_SHOWWINDOW);
+			}
+
+		}
+
+		private void label1_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button5_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button8_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void label3_Click(object sender, EventArgs e)
+		{
+
+		}
+		private void RestartExplorer()
+		{
+			try
+			{
+				ProcessStartInfo psi = new ProcessStartInfo
+				{
+					FileName = "cmd.exe",
+					Arguments = "/C START explorer.exe",
+					WindowStyle = ProcessWindowStyle.Hidden,
+					CreateNoWindow = true,
+					UseShellExecute = false
+				};
+
+				Process.Start(psi);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Error restarting explorer.exe: " + ex.Message);
+			}
+		}
+		private void button13_Click(object sender, EventArgs e)
+		{
+
+			try
+			{
+				string url = "https://www.google.com";
+				ProcessStartInfo psi = new ProcessStartInfo
+				{
+					FileName = url,
+					UseShellExecute = true
+				};
+				System.Diagnostics.Process.Start(psi);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Error opening the browser: " + ex.Message);
+			}
+
+
+		}
+		private const string HostsFilePath = @"C:\Windows\System32\drivers\etc\hosts";
+		private readonly List<string> BlockedWebsites = new List<string>
+{
+	"www.messenger.com",
+	"www.instagram.com",
+	"www.facebook.com",
+	"web.whatsapp.com",
+	"chat.openai.com",
+	"www.telegram.com",
+	"www.signal.com",
+	"web.snapchat.com"
+};
+
+		private void BlockWebsites()
+		{
+			try
+			{
+				var hostsContent = File.ReadAllText(HostsFilePath);
+
+				foreach (var website in BlockedWebsites)
+				{
+					hostsContent += $"\n127.0.0.1 {website}";
+				}
+
+				File.WriteAllText(HostsFilePath, hostsContent);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Error blocking websites: " + ex.Message);
+			}
+		}
+			private void UnblockWebsites()
+			{
+				try
+				{
+					var hostsContent = File.ReadAllText(HostsFilePath);
+					var lines = hostsContent.Split('\n');
+				hostsContent = string.Join("\n", lines.Where(line => !BlockedWebsites.Any(website => line.Contains(website))));
+
+				File.WriteAllText(HostsFilePath, hostsContent);
+				}
+				catch (Exception ex)
+				{
+				MessageBox.Show("Error unblocking websites: " + ex.Message);
+				}
+			}
+		private void timer1_Tick(object sender, EventArgs e)
+			{
+			if (timeLeft > TimeSpan.Zero)
+			{
+				timeLeft = timeLeft.Subtract(TimeSpan.FromSeconds(1));
+				label1.Text = timeLeft.ToString(@"hh\:mm\:ss");
+			}
+			else
+			{
+				timer1.Stop();
+				label1.Text = "00:00:00";
+				MessageBox.Show("Time's up!");
+			}
+		}
+
+		private void Docker_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void pictureBox1_Click(object sender, EventArgs e)
+		{
+			Docker docker = new Docker();
+			docker.Close();
+		}
+	}
+
+}
+
